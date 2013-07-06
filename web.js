317d923398bf9510df29bb6eeacd9cfaac75e2e5
John Richter <johndavidrichter@gmail.com>
2013-07-06 07:34:58 +0000

    using buffers now.

--- web.js
+++ web.js
@@ -1,21 +1,25 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
-var indexContents;
+
+
+/* OLD CODE
 fs.readFile('index.html', 'utf8', function (err,data) {
-    indexContents = console.log(data);
-
+  indexContents = data;
+  console.log(data);
 });
 
+if (indexContents)
+    console.log("data is " + indexContents);
+else
+    console.log("data doesn't exist outside of scope!");
+*/
 
 app.get('/', function(request, response) {
-  response.send(indexContents);
-});
-/*
-app.get('/', function(request, response) {
-  response.send("test");
-});*/
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 
 var port = process.env.PORT || 5000;
